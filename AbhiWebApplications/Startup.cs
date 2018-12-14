f8e19cfb9a109dcc1527709b9d7a736a948fd0dd
Subba <abinaya.subbu@siemens-healthineers.com>
2018-12-14 13:45:02 +0530

    Initial Branch Comitted

---
+++ AbhiWebApplications/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AbhiWebApplications.Startup))]
+namespace AbhiWebApplications
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
